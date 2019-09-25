0140a934a40c69554835a211869835a3b8ed909d
Liuliyisui <m18628156073@163.com>
2019-09-25 19:51:55 +0800

    创建项目第一次提交

---
+++ springboot-meeting/src/main/java/com/qf/meeting/springbootmeeting/SpringbootMeetingApplication.java
@@ -0,0 +1,17 @@
+package com.qf.meeting.springbootmeeting;
+
+import com.qf.pojo.User;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.jws.soap.SOAPBinding;
+
+@SpringBootApplication
+public class SpringbootMeetingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMeetingApplication.class, args);
+
+    }
+
+}
